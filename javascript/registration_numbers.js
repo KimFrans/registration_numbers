9ebc084075c2d2f04b9894d3df9feb2df4ffcedf
Kim Frans <kimfrans34@gmail.com>
2021-05-05 11:22:16 +0200

    html

--- javascript/registration_numbers.js
+++ javascript/registration_numbers.js
@@ -7,8 +7,8 @@ var errorMessage = document.querySelector(".errorMessage")
 var counterDis = document.querySelector(".counterValue")
 var message = document.querySelector(".message")
 var regDisplay = document.querySelector(".reg1")
-var regDisplay2 = document.querySelector(".reg2")
-var regDisplay3 = document.querySelector(".reg3")
+// var regDisplay2 = document.querySelector(".reg2")
+// var regDisplay3 = document.querySelector(".reg3")
 var checkedRadio1 = document.querySelector("input[name='town']:checked");
 checkedRadio1.checked = false
 
